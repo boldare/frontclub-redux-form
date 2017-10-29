b5ef943004549a4c7120b72a5995dc2aaf9ad4e2
MichalCiesliczka <michal.ciesliczka@xsolve.com>
2017-10-29 18:38:41 +0100

    Add simple redux form

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-const Home = () => (
-  <p>
-    Home!
-  </p>
-)
+const Home = (props) => {
+  const { handleSubmit } = props;
 
-export default Home
+  const sendDataToApi = (formData) => {
+    console.log(formData)
+  };
+
+  return (
+    <div>
+      <h2>Simple home form</h2>
+      <form onSubmit={handleSubmit(sendDataToApi)}>
+        <Field
+          name="firstName"
+          component="input"
+          placeholder="Put your first name"
+        />
+        <button type="submit">Submit form</button>
+      </form>
+    </div>
+  );
+};
+
+export default reduxForm({
+  form: 'simple-form'
+})(Home);
