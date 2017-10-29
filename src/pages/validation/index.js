1bc9ac49ee7e93895fe39a05d676a2cb70ca69e0
MichalCiesliczka <michal.ciesliczka@xsolve.com>
2017-10-29 20:11:41 +0100

    Add validation

--- src/pages/validation/index.js
+++ src/pages/validation/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field, SubmissionError } from 'redux-form';
+
+import Input from '../../components/Input'
+
+const validate = values => {
+  let errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = 'Please insert first name'
+  }
+
+  return errors;
+}
+
+const isGreaterThan10 = value => {
+  if (value && value < 10) {
+    return "Tooooo young"
+  }
+
+  return undefined;
+}
 
 const Validation = (props) => {
   const { handleSubmit, dirty, reset, submitting } = props;
@@ -8,7 +28,12 @@ const Validation = (props) => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         console.log(formData);
-        reject();
+        if (formData.email && 'asd@asd.pl' === formData.email) {
+          reject (new SubmissionError({
+            email: 'That is fake'
+          }))
+        }
+        resolve();
       }, 2000);
     })
   };
@@ -19,8 +44,24 @@ const Validation = (props) => {
       <form onSubmit={handleSubmit(sendDataToApi)}>
         <Field
           name="firstName"
-          component="input"
+          component={Input}
           placeholder="Put your first name"
+          label="First Name"
+        />
+        <Field
+          name="email"
+          component={Input}
+          type="email"
+          placeholder="Put your email here"
+          label="Email"
+        />
+        <Field
+          name="age"
+          component={Input}
+          type="number"
+          placeholder="Put your age here"
+          label="Age"
+          validate={isGreaterThan10}
         />
         <button
           onClick={reset}
@@ -41,5 +82,6 @@ const Validation = (props) => {
 };
 
 export default reduxForm({
-  form: 'simple-form'
+  form: 'simple-form',
+  validate
 })(Validation);
