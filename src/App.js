704c0d1a00ea6813831413c2a045aa83520cf59e
MichalCiesliczka <michal.ciesliczka@xsolve.com>
2017-10-29 19:00:55 +0100

    Prepare for validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
 import Home from './pages/home'
+import Validation from './pages/validation'
 
 import './App.css';
 
@@ -14,7 +15,7 @@ const App = () => (
       <NavLink to="/validation" className="link" activeClassName="is-active">Validation</NavLink>
       <NavLink to="/initial-values" className="link" activeClassName="is-active">Initial Values</NavLink>
     </nav>
-    {/* <Route path="/validation" component={Validation} /> */}
+    <Route exact path="/validation" component={Validation} />
     {/* <Route path="/initial-values" component={InitialValues} /> */}
     <Route exact path="/" component={Home} />
   </div>
---
+++ src/pages/validation/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+const Validation = (props) => {
+  const { handleSubmit, dirty, reset, submitting } = props;
+
+  const sendDataToApi = (formData) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(formData);
+        reject();
+      }, 2000);
+    })
+  };
+
+  return (
+    <div>
+      <h2>Validation form</h2>
+      <form onSubmit={handleSubmit(sendDataToApi)}>
+        <Field
+          name="firstName"
+          component="input"
+          placeholder="Put your first name"
+        />
+        <button
+          onClick={reset}
+          type="button"
+          disabled={!dirty || submitting}
+        >
+          Reset form
+        </button>
+        <button
+          type="submit"
+          disabled={!dirty || submitting}
+        >
+          Submit form
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default reduxForm({
+  form: 'simple-form'
+})(Validation);
