f5bde0b2a245c199495094348279e136e06aa951
MichalCiesliczka <michal.ciesliczka@xsolve.pl>
2018-02-16 11:56:19 +0100

    Tempo

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,5 @@
 export const sendDataToApi = (formData) => {
   console.log(formData);
-  // return new Promise((resolve, reject) => {
-  //   setTimeout(() => {
-  //     console.log(formData);
-  //     // ... do stuff;
-
-  //     resolve();
-  //   }, 2000);
-  // })
 };
 
 export default sendDataToApi;
