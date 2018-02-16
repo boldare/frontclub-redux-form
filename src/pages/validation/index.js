aae6cbe5507d5f95ae4e6470187778fafdbef344
MichalCiesliczka <michal.ciesliczka@xsolve.pl>
2018-02-16 09:48:20 +0100

    Remove commented code

--- src/pages/validation/index.js
+++ src/pages/validation/index.js
@@ -8,56 +8,8 @@ import { sendDataToApi } from '../../utils/api';
 import Input from '../../components/Input'
 import FormButtons from '../../components/FormButtons'
 
-//#region walidate
-// const validate = values => {
-//   let errors = {};
-
-//   if (!values.myName) {
-//     errors.myName = 'You better insert my name'
-//   }
-
-//   return errors;
-// }
-//#endregion
-
-//#region warn
-// const warn = values => {
-//   let warnings = {};
-
-//   // INSERT NAME
-//   if (values.myName && "" === values.myName) {
-//     warnings.myName = 'You\'re goddamn right!'
-//   }
-
-//   return warnings;
-// }
-//#endregion
-
-//#region isGreater
-// const isGreaterThan10 = value => {
-//   if (value && value < 10) {
-//     return "Way tooo young to smoke crystals. Get out of here!"
-//   }
-
-//   return undefined;
-// }
-//#endregion
-
 const Validation = (props) => {
   const { handleSubmit, dirty, reset, submitting, submitFailed, submitSucceeded } = props;
-
-  // #region handle server validation
-  // const saveAndHandleServerValidation = (formData) => {
-  //   return new Promise((resolve, reject) => {
-  //     sendDataToApi(formData)
-  //       .then((res) => {
-  //         resolve();
-  //       }, (err) => {
-  //         reject(new SubmissionError(err));
-  //       });
-  //   })
-  // };
-  // #endregion
 
   return (
     <div>
